838819de0e556d10058e70ee84dd3c52dc1bd7b3
Saladin-B <saladeen.baalache@gmail.com>
2026-02-20 17:37:24 +0000

    install gunicorn and freeze requirements

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--8#oi5(+k7tffr@r5v0pute0n35^f$i2oq#)^8falm7lyuk#b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
