73694e17c98ba9a8131dbe4870670f837ab0421d
Saladin-B <saladeen.baalache@gmail.com>
2026-02-21 21:29:17 +0000

    debug set to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--8#oi5(+k7tffr@r5v0pute0n35^f$i2oq#)^8falm7lyuk#b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ci-website-blog-bfd23a882f38.herokuapp.com']
 
