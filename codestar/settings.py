231469ec90d8e96e1934c4573fa4de611e4eda45
Saladin-B <saladeen.baalache@gmail.com>
2026-02-23 00:49:35 +0000

    debug to true then new import user model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ci-website-blog-bfd23a882f38.herokuapp.com']
 
