a7c6376e8c476292d81702f03a91cec366794a70
Saladin-B <saladeen.baalache@gmail.com>
2026-02-23 01:02:17 +0000

    Slug field post model

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
