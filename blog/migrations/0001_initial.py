f3cf544a224afcb985b095592c6a4b3f5711af0d
Saladin-B <saladeen.baalache@gmail.com>
2026-02-25 16:09:17 +0000

    Fix typo in 0001_initial - experts to excerpt

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('content', models.TextField()),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
                 ('status', models.IntegerField(choices=[(0, 'Draft'), (1, 'Published')], default=0)),
-                ('experts', models.TextField(blank=True, max_length=200)),
+                ('excerpt', models.TextField(blank=True, max_length=200)),
                 ('updated_on', models.DateTimeField(auto_now=True)),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
                 
