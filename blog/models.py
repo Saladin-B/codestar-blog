a762aae30118bdde5b9c93ae55bf965219fe7dab
Saladin-B <saladeen.baalache@gmail.com>
2026-02-28 16:42:38 +0000

    null to true

--- blog/models.py
+++ blog/models.py
@@ -16,17 +16,17 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(max_length=200, blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self) -> str:
         return self.title
     
-    class Comment(models.Model):
-        post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
-        author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-        body = models.TextField()
-        approved = models.BooleanField(default=False)
-        created_on = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
         
-        def __str__(self) -> str:
-            return f"Comment on {self.post}"
+    def __str__(self) -> str:
+        return f"Comment on {self.post}"
